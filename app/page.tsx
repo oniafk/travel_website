ed223d5b351198bde705b43459d5eabe2751babf
oniafk <manufelipearias@gmail.com>
2025-02-11 10:21:11 +1100

    created components for create structure on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import { Camp } from "@/components/Camp";
+import { Features } from "@/components/Features";
+import GerApp from "@/components/GerApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
+
 export default function Home() {
-  return <h1 className="font-bold text-6xl text-teal-600"> Hello world</h1>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GerApp />
+    </>
+  );
 }
---
+++ components/GerApp.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GerApp = () => {
+  return <div>GerApp</div>;
+};
+
+export default GerApp;
