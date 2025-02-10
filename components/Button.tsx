0f34b47b8604d29d7e0023eb7198ce15f554edfc
oniafk <manufelipearias@gmail.com>
2025-02-11 02:27:51 +1100

    added navbar and button component, also added resources svg images

---
+++ components/Button.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import React from "react";
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  icon?: string;
+  variant: "btn_dark_green";
+};
+
+export const Button = ({ type, title, icon, variant }: ButtonProps) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} width={20} height={20} />}
+      <label className="bold-16 whitespace-nowrap">{title}</label>
+    </button>
+  );
+};
